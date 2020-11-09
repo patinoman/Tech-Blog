ddac8788d9a2884a4e9952b10b6ff7502f37cbb5
patinoman <68838876+patinoman@users.noreply.github.com>
2020-11-09 01:08:39 -0800

    added Routes/api

---
+++ controllers/index.js
@@ -0,0 +1,14 @@
+const router = require('express').Router();
+const apiRoutes = require('./api');
+const homeRoutes = require('./home-routes.js');
+const dashboardRoutes = require('./dashboard-routes.js');
+
+router.use('/dashboard', dashboardRoutes);
+router.use('/', homeRoutes);
+router.use('/api', apiRoutes);
+router.use((req, res) => {
+    console.log('no routes hit');
+    res.status(404).end();
+});
+
+module.exports = router;
